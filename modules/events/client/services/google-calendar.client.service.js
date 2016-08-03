8d3553817bacc297df1e53bd2d8a30b5efab94a4
Hanamant-Kadlimatti <hanamantrkadlimatti@gmail.com>
2016-08-03 16:30:38 +0530

    domain name

--- modules/events/client/services/google-calendar.client.service.js
+++ modules/events/client/services/google-calendar.client.service.js
@@ -13,7 +13,7 @@ angular.module('GoogleCalendarService', [], function($provide){
 		
 		// console.log('MY URL===>' +baseUrl);
 		 
-		var baseUrl = 'https://mylatestdentalproject.herokuapp.com';
+		var baseUrl = 'https://mylatestdentalproject.herokuapp.com/';
  
 		return {
 			load: function(){
