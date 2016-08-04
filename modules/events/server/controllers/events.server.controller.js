c746744824ac7f681773ed852a09b17074b262bf
Hanamant-Kadlimatti <hanamantrkadlimatti@gmail.com>
2016-08-04 14:39:42 +0530

    domain name

--- modules/events/server/controllers/events.server.controller.js
+++ modules/events/server/controllers/events.server.controller.js
@@ -6,45 +6,45 @@ var User = require('mongoose').model('User');
 var path = require('path');
 var errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
-  // Load the twilio module
-         var twilio = require('twilio');
+//   // Load the twilio module
+//          var twilio = require('twilio');
  
-        // Create a new REST API client to make authenticated requests against the
-        // twilio back end
-          var client = new twilio.RestClient('AC1a0432d220e8240cae1acf4b39ff04b4', '28ad85cfa30f29acb55e21fd179db977');
+//         // Create a new REST API client to make authenticated requests against the
+//         // twilio back end
+//           var client = new twilio.RestClient('AC1a0432d220e8240cae1acf4b39ff04b4', '28ad85cfa30f29acb55e21fd179db977');
 
    
         
-        var sendSms = function(){
+//         var sendSms = function(){
 
        
 
-        // Pass in parameters to the REST API using an object literal notation. The
-         // REST client will handle authentication and response serialzation for you.
-         client.sms.messages.create({
-              // to:'+919663398669',
-               to:'+918792955198',   // This is my original number
-               from:'+17865286119', //I got this number from twilio
-                body:' Your Appointment is Confirmed  Thank You'
-           }, function(error, message) {
-          // The HTTP request to Twilio will run asynchronously. This callback
-          // function will be called when a response is received from Twilio
-          // The "error" variable will contain error information, if any.
-          // If the request was successful, this value will be "falsy"
-           if (!error) {
-               // The second argument to the callback will contain the information
-               // sent back by Twilio for the request. In this case, it is the
-               // information about the text messsage you just sent:
-                  console.log('Success! The SID for this SMS message is:');
-                   console.log(message.sid);
+//         // Pass in parameters to the REST API using an object literal notation. The
+//          // REST client will handle authentication and response serialzation for you.
+//          client.sms.messages.create({
+//               // to:'+919663398669',
+//                to:'+918792955198',   // This is my original number
+//                from:'+17865286119', //I got this number from twilio
+//                 body:' Your Appointment is Confirmed  Thank You'
+//            }, function(error, message) {
+//           // The HTTP request to Twilio will run asynchronously. This callback
+//           // function will be called when a response is received from Twilio
+//           // The "error" variable will contain error information, if any.
+//           // If the request was successful, this value will be "falsy"
+//            if (!error) {
+//                // The second argument to the callback will contain the information
+//                // sent back by Twilio for the request. In this case, it is the
+//                // information about the text messsage you just sent:
+//                   console.log('Success! The SID for this SMS message is:');
+//                    console.log(message.sid);
  
-                  console.log('Message sent on:');
-                   console.log(message.dateCreated);
-               } else {
-                 console.log('Oops! There was an error.');
-             }
-           });
-         }
+//                   console.log('Message sent on:');
+//                    console.log(message.dateCreated);
+//                } else {
+//                  console.log('Oops! There was an error.');
+//              }
+//            });
+//          }
         
         
 
