9f313774460288932ba384229974c7ca61ff80ad
bhuvansalanke <bhuvansalanke@gmail.com>
2016-09-16 16:57:17 +0530

    chsnges

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,33 +4,33 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'public/lib/fullcalendar/dist/fullcalendar.css',
-        'public/lib/jquery-timepicker-jt/jquery.timepicker.css',
-        'public/lib/font-awesome/font-awesome.css',
-        'public/lib/angular-input-stars-directive/angular-input-stars.css',
+        'public/lib/fullcalendar/dist/fullcalendar.min.css',
+        'public/lib/jquery-timepicker-jt/jquery.timepicker.min.css',
+        'public/lib/font-awesome/font-awesome.min.css',
+        'public/lib/angular-input-stars-directive/angular-input-stars.min.css',
         'public/lib/angularMultipleSelect/build/multiple-select.min.css',
-        'public/lib/fullcalendar-scheduler/dist/scheduler.css'
+        'public/lib/fullcalendar-scheduler/dist/scheduler.min.css'
       ],
       js: [
-        'public/lib/angular-route/angular-route.js',
-        'public/lib/angular-resource/angular-resource.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.js',
-        'public/lib/angular-ui-utils/ui-utils.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-file-upload/dist/angular-file-upload.js',
-        'public/lib/angular-ui-calendar/src/calendar.js',
-        'public/lib/moment/moment.js',
-        'public/lib/angular-moment/angular-moment.js',
-        'public/lib/fullcalendar/dist/fullcalendar.js',
-        'public/lib/angular-cookies/angular-cookies.js',
+        'public/lib/angular-route/angular-route.min.js',
+        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-ui-utils/ui-utils.min.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
+        'public/lib/angular-ui-calendar/src/calendar.min.js',
+        'public/lib/moment/moment.min.js',
+        'public/lib/angular-moment/angular-moment.min.js',
+        'public/lib/fullcalendar/dist/fullcalendar.min.js',
+        'public/lib/angular-cookies/angular-cookies.min.js',
         'public/lib/angular-filter/dist/angular-filter.min.js',
         'public/lib/jquery-timepicker-jt/jquery.timepicker.min.js',
-        'public/lib/angular-jquery-timepicker/src/timepickerdirective.js',
-        'public/lib/angular-input-stars-directive/angular-input-stars.js',
+        'public/lib/angular-jquery-timepicker/src/timepickerdirective.min.js',
+        'public/lib/angular-input-stars-directive/angular-input-stars.min.js',
         'public/lib/angularMultipleSelect/build/multiple-select.min.js',
         'public/lib/angular-strap/dist/angular-strap.min.js', 
         'public/lib/angular-strap/dist/angular-strap.tpl.min.js',
-        'public/lib/fullcalendar-scheduler/dist/scheduler.js'
+        'public/lib/fullcalendar-scheduler/dist/scheduler.min.js'
       ]
     },
     css: 'public/dist/application.min.css',
