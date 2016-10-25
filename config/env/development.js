46faccede49c2f06459d240fa4030ab6e8aba6c9
Hanamant-Kadlimatti <hanamantrkadlimatti@gmail.com>
2016-10-25 18:45:45 +0530

    changes

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://confident:confident13@ds031617.mlab.com:31617/confidentbookingdb',
     options: {
       user: '',
       pass: ''
