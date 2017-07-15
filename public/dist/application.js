84b78958be34a998fe417afe4849b187f6247aff
confident <confidenthospital@gmail.com>
2017-07-15 12:16:00 +0530

    changes

--- public/dist/application.js
+++ public/dist/application.js
@@ -2295,7 +2295,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
         $scope.authentication.user = response.data;
 
         // And redirect to the previous or home page
-        $state.go($state.previous.state.name || 'home', $state.previous.params);
+        $state.go($state.previous.state.name || 'events.main', $state.previous.params);
       }, function (response) {
         $scope.error = response.data.message;
       });
